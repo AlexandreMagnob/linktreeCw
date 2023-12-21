e65aac58ee7f9d4ee1d5a36bd747b9e3f043f30e
Alexandre Magno <alexandre.magno@cardapioweb.com>
2023-12-21 14:12:00 -0300

    Formulario quase pronto, falta backand

---
+++ my-appp/pages/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const dashboard = () => {
+  return (
+    <>
+      <h1 className='text-white bg-gray-600 rounded-md'>dashboard</h1>
+    </>
+  )
+}
+
+export default dashboard
